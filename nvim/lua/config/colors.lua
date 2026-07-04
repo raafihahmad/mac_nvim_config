940381def48769cc39cb26f3d667cd8d09e946e1
raafihahmad <raafihahmad@gmail.com>
2026-07-04 13:11:55 +0530

    Create colors.lua

---
+++ nvim/lua/config/colors.lua
@@ -0,0 +1,10 @@
+-- Sonokai
+vim.g.sonokai_style = "maia"
+vim.cmd.colorscheme("sonokai")
+
+-- Gruvbox
+-- require("gruvbox").setup({})
+-- vim.cmd.colorscheme("gruvbox")
+
+-- Gruvbox Flat
+-- vim.cmd.colorscheme("gruvbox-flat")
