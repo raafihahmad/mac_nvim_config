beff8a512515e45951297c8aa04a803197906804
raafihahmad <raafihahmad@gmail.com>
2026-07-04 13:14:27 +0530

    Create init.lua

---
+++ nvim/plugins/init.lua
@@ -0,0 +1,20 @@
+vim.pack.add({
+  "https://github.com/sainnhe/sonokai",
+  "https://github.com/ellisonleao/gruvbox.nvim",
+  "https://github.com/eddyekofo94/gruvbox-flat.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/nvim-telescope/telescope.nvim",
+  "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/hrsh7th/nvim-cmp",
+  "https://github.com/hrsh7th/cmp-nvim-lsp",
+  "https://github.com/hrsh7th/cmp-buffer",
+  "https://github.com/hrsh7th/cmp-path",
+  "https://github.com/L3MON4D3/LuaSnip",
+  "https://github.com/saadparwaiz1/cmp_luasnip",
+  "https://github.com/RRethy/base16-nvim"
+})
+
+require("plugins.lsp")
+require("plugins.completion")
+require("plugins.telescope_config")
